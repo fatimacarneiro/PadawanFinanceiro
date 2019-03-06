461f9f07986bf48cc087837158603cb42f4a36e4
jcestaro <joseaugusto.conceicao@bluesoft.com.br>
2019-03-06 16:57:50 -0300

    #181425/adicionando-popup-filtroAvancado

--- www/controllers/movimentosController.js
+++ www/controllers/movimentosController.js
@@ -1,6 +1,6 @@
 angular.module('starter').controller('tabelaController', function($scope, $location, consultaMovimentosService, $ionicPopup){
-
-    $scope.filtra = function(event){
+    
+    $scope.filtraDescricao = function(event){
 
         this.tabelaExibida = this.listaMovimento.filter((itemTabela) => itemTabela.description.match(event, 'i'));
     }
@@ -27,15 +27,51 @@ angular.module('starter').controller('tabelaController', function($scope, $locat
     }
 
     $scope.excluir = function(movimentoSelecionado) {
-        var confirmPopup = $ionicPopup.confirm({
-            title: 'Excluir Movimento',
-            template: 'Tem certeza que deseja excluir esse movimento?'
-          });
-       
-          confirmPopup.then(function(res) {
-            if(res) {
-                consultaMovimentosService.apagar(movimentoSelecionado.id)
-            } 
-          });
+    var confirmPopup = $ionicPopup.confirm({
+        title: 'Excluir Movimento',
+        template: 'Tem certeza que deseja excluir esse movimento?'
+        });
+    
+        confirmPopup.then(function(res) {
+        if(res) {
+            consultaMovimentosService.apagar(movimentoSelecionado.id)
+        } 
+        });
+    }
+
+    $scope.filtroAvancado = function(){
+        
+        $scope.movimento = {}
+
+        $ionicPopup.show({
+            templateUrl: 'templates/filtroAvancado.html',
+            title: 'Filtro Avançado',
+            subTitle: 'Selecione os dados para filtrar',
+            scope: $scope,
+            buttons: [
+            { text: 'Cancelar' },
+            {
+                text: '<b>Filtrar</b>',
+                type: 'button-positive',
+                onTap: function() {
+                    if ($scope.movimento.date) {
+                        let dia=$scope.movimento.date.getDate();
+                        let mes=$scope.movimento.date.getMonth();
+                        let ano=$scope.movimento.date.getFullYear();
+                        $scope.data = dia + '/' + (mes++) + '/' + ano;
+                        
+                        console.log($scope.movimento.value)
+                        console.log($scope.tabelaExibida = $scope.listaMovimento.filter((itemTabela) => itemTabela.date.match($scope.movimento.date, 'g')));
+                    }
+                    if ($scope.movimento.value){
+                        console.log($scope.tabelaExibida = $scope.listaMovimento.filter((itemTabela) => itemTabela.value.match($scope.movimento.value, 'g')));
+                    }
+                    if ($scope.movimento.type){
+                        console.log($scope.tabelaExibida = $scope.listaMovimento.filter((itemTabela) => itemTabela.type.match($scope.movimento.type, 'g')));
+                    }
+                }
+            }
+            ]
+        })
     }
 })
